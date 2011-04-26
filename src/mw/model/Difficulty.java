610696e1c9412ce984f9c59bd878106552c72da6
European <mail@nils-werner.com>
2011-04-26 17:34:32 +0200

    neu

--- src/mw/model/Difficulty.java
+++ src/mw/model/Difficulty.java
@@ -3,33 +3,11 @@ package mw.model;
 public enum Difficulty {
 	EINFACH, MITTEL, SCHWER, BENUTZERDEFINIERT;
 
+	
 	public static Difficulty fromString(String name) {
 		return getEnumFromString(Difficulty.class, name);
 	}
-
-	public String toString() {
-		String ret = "";
-
-		switch (this) {
-		case EINFACH:
-			ret = "einfach";
-			break;
-		case MITTEL:
-			ret = "mittel";
-			break;
-		case SCHWER:
-			ret = "schwer";
-			break;
-		case BENUTZERDEFINIERT:
-			ret = "benutzerdefiniert";
-			break;	
-		default:
-			break;
-		}
-
-		return ret;
-	}
-
+	
 	/**
 	 * A common method for all enums since they can't have another base class
 	 * 
